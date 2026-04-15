f36fbc65c54344ffbc28761338ed1cf3a7e81146
shashideepika-06 <shashideepika50@gmail.com>
2026-04-15 16:13:55 +0530

    Fixed CSS import case mismatch

--- src/pages/CategoryServices.jsx
+++ src/pages/CategoryServices.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom";
-import "./categoryServices.css";
+import "./CategoryServices.css";
 import bg from "./../assets/home.png";
 
 // --- SALON ---
